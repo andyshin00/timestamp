7c6f29327038bfe2ae785af5283871368df09788
andrew shin <shinandy22@gmail.com>
2026-08-16 13:07:05 -0700

    added video schema

--- database/schema.ts
+++ database/schema.ts
@@ -1,7 +1,30 @@
-import { pgTable, serial, text } from "drizzle-orm/pg-core";
+import {
+  pgTable,
+  serial,
+  text,
+  integer,
+  timestamp,
+  jsonb,
+} from "drizzle-orm/pg-core";
 
-export const usersTable = pgTable("users", {
+export const usersSchema = pgTable("users", {
   id: serial("id").primaryKey(),
   email: text("email").unique().notNull(),
   hashedPassword: text("hashedPassword").notNull(),
+});
+
+export const videosSchema = pgTable("videos", {
+  id: serial("id").primaryKey(),
+  userId: integer("userId")
+    .references(() => usersSchema.id, { onDelete: "cascade" })
+    .notNull(),
+  youtubeId: text("youtubeId").notNull(),
+  title: text("title").notNull(),
+  channel: text("channel"),
+  thumbnailUrl: text("thumbnailUrl"),
+  transcript: text("transcript"),
+  timestamps: jsonb("timestamps")
+    .$type<{ time: number; label: string }[]>()
+    .notNull(),
+  createdAt: timestamp("createdAt").defaultNow().notNull(),
 });
