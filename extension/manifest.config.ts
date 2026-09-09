9405b01f06c3ea16801379b882dfd40ea0c67331
andrew shin <shinandy22@gmail.com>
2026-09-09 04:35:04 -0700

    base extension

---
+++ extension/manifest.config.ts
@@ -0,0 +1,19 @@
+import { defineManifest } from "@crxjs/vite-plugin";
+import pkg from "./package.json";
+
+export default defineManifest({
+  manifest_version: 3,
+  name: pkg.name,
+  version: pkg.version,
+  icons: {
+    48: "public/logo.png",
+  },
+  action: {
+    default_icon: {
+      48: "public/logo.png",
+    },
+    default_popup: "src/popup/index.html",
+  },
+  permissions: ["storage", "activeTab"],
+  host_permissions: ["http://localhost:3000/*"],
+});
---
+++ extension/src/popup/App.tsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="w-90 p-4">
+      <h1 className="text-lg font-bold">Timestamps</h1>
+    </div>
+  );
+}
